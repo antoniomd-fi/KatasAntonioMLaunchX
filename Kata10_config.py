aaf814b100438b9ae7373dd65ef9f1f757fcd5d5
Antonio Martin <90221784+antoniomd-fi@users.noreply.github.com>
2022-02-13 20:54:59 -0600

    Add files via upload

---
+++ Kata10_config.py
@@ -0,0 +1,13 @@
+def main ():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+    except PermissionError:
+        print("Found config.txt but don't have the necessary permissions")
+
+
+if __name__ == '__main__':
+    main()
